8dbcb97dd638515a4449da099fa6cdfe18ac84e6
cmscheffer <cassioscheffer87@gmail.com>
2025-10-31 13:48:20 +0000

    fix: remove mensagem irritante 'Você já está autenticado'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :remove_already_authenticated_flash
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
+
+  # Remove a mensagem irritante "Você já está autenticado"
+  def remove_already_authenticated_flash
+    if flash[:alert] == "Você já está autenticado."
+      flash.delete(:alert)
+    end
   end
 end
